9e898a11bc841fe9f0a5d5fca682e992373f3c91
Vishaal Kanna Sivakumar <90156679+Vishaal-Kanna@users.noreply.github.com>
2022-10-12 16:02:56 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,9 @@
 /**
  * @file main.cpp
- * @authors Rishabh Singh, Vishaal Kanna Sivakumar
- * @brief main function for PID controller
+ * @authors Sahruday Patti, Vishaal Kanna Sivakumar
+ * @brief main function for Human Tracking
  * @version 0.1
- * @date 2022-10-02
+ * @date 2022-10-12
  *
  * @copyright Copyright (c) 2022
  *
