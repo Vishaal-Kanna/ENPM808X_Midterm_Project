fa2e02c14ae34eedc261caea28f3ad8c7bd5c3b2
Vishaal-Kanna <svishaalkanna@gmail.com>
2022-10-13 20:43:38 -0400

    Adding Dev branch

---
+++ build/app/CMakeFiles/ACME.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "ACME"
+  "ACME.pdb"
+  "CMakeFiles/ACME.dir/ACME_robot.cpp.o"
+  "CMakeFiles/ACME.dir/ACME_robot.cpp.o.d"
+  "CMakeFiles/ACME.dir/Detection_module.cpp.o"
+  "CMakeFiles/ACME.dir/Detection_module.cpp.o.d"
+  "CMakeFiles/ACME.dir/Tracking_module.cpp.o"
+  "CMakeFiles/ACME.dir/Tracking_module.cpp.o.d"
+  "CMakeFiles/ACME.dir/Transformation_module.cpp.o"
+  "CMakeFiles/ACME.dir/Transformation_module.cpp.o.d"
+  "CMakeFiles/ACME.dir/main.cpp.o"
+  "CMakeFiles/ACME.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ACME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/cpp-test.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-test.dir/__/app/ACME_robot.cpp.o"
+  "CMakeFiles/cpp-test.dir/__/app/ACME_robot.cpp.o.d"
+  "CMakeFiles/cpp-test.dir/__/app/Detection_module.cpp.o"
+  "CMakeFiles/cpp-test.dir/__/app/Detection_module.cpp.o.d"
+  "CMakeFiles/cpp-test.dir/__/app/Tracking_module.cpp.o"
+  "CMakeFiles/cpp-test.dir/__/app/Tracking_module.cpp.o.d"
+  "CMakeFiles/cpp-test.dir/__/app/Transformation_module.cpp.o"
+  "CMakeFiles/cpp-test.dir/__/app/Transformation_module.cpp.o.d"
+  "CMakeFiles/cpp-test.dir/main.cpp.o"
+  "CMakeFiles/cpp-test.dir/main.cpp.o.d"
+  "CMakeFiles/cpp-test.dir/test.cpp.o"
+  "CMakeFiles/cpp-test.dir/test.cpp.o.d"
+  "cpp-test"
+  "cpp-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
